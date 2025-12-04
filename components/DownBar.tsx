e41c31ebb74e50255e6ffb893c6c86905467af95
k4hr <alecsandr-7a@yandex.ru>
2025-12-04 12:51:39 +0300

    Update DownBar.tsx

--- components/DownBar.tsx
+++ components/DownBar.tsx
@@ -3,6 +3,7 @@
 
 import VrachiOnlineBlock from './DownBarUtil/vrachionline';
 import OvrachaxBlock from './DownBarUtil/ovrachax';
+import VozmojnostiBlock from './DownBarUtil/vozmojnosti';
 
 /**
  * Общий DownBar — просто блок в конце страницы.
@@ -13,12 +14,16 @@ export default function DownBar() {
       <section className="downbar">
         <VrachiOnlineBlock />
         <OvrachaxBlock />
+        <VozmojnostiBlock />
       </section>
 
       <style jsx>{`
         .downbar {
           margin-top: 16px;
           margin-bottom: 4px;
+          display: flex;
+          flex-direction: column;
+          gap: 24px;
         }
       `}</style>
     </>
