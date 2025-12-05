3bb8736f59eaa25b74528fa3cd38b800da119637
k4hr <alecsandr-7a@yandex.ru>
2025-12-05 05:19:17 +0300

    Update vozmojnosti.tsx

--- components/DownBarUtil/vozmojnosti.tsx
+++ components/DownBarUtil/vozmojnosti.tsx
@@ -33,7 +33,7 @@ const MODES: ModeCard[] = [
     id: 2,
     title: 'Диагноз+',
     subtitle: 'Популярный режим',
-    price: 'от ₽',
+    price: 'от 599 ₽',
     color: 'gold',
     features: [
       { good: true, text: 'Мгновенная публикация вопроса' },
