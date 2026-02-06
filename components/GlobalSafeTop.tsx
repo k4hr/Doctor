4926a345403e921affc49b1c98582abeced90a38
k4hr <alecsandr-7a@yandex.ru>
2026-02-06 03:03:25 +0300

    Update GlobalSafeTop.tsx

--- components/GlobalSafeTop.tsx
+++ components/GlobalSafeTop.tsx
@@ -5,8 +5,8 @@ export default function GlobalSafeTop() {
   return (
     <style jsx global>{`
       :root {
-        /* ✅ ФОЛБЭК. Если TelegramNoSwipeInit поставит --lm-safe-top, он перекроет это значение.
-           Если НЕ поставит — будет работать iOS safe-area. */
+        /* Фолбэк: iOS safe-area.
+           TelegramNoSwipeInit при наличии tg переопределит --lm-safe-top точнее. */
         --lm-safe-top: env(safe-area-inset-top, 0px);
       }
     `}</style>
