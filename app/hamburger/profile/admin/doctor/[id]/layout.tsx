94084e0a6a0e0c2ad8265a6c0a7d1b222c103617
k4hr <alecsandr-7a@yandex.ru>
2026-02-06 07:25:10 +0300

    Create layout.tsx

---
+++ app/hamburger/profile/admin/doctor/[id]/layout.tsx
@@ -0,0 +1,10 @@
+/* path: app/hamburger/profile/admin/doctor/[id]/layout.tsx */
+import type { ReactNode } from 'react';
+
+export const viewport = {
+  themeColor: '#ffffff',
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return children;
+}
