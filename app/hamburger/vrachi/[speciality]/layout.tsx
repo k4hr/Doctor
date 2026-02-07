30541e5f416bcc79cb9a82d9f28f456294424379
k4hr <alecsandr-7a@yandex.ru>
2026-02-08 00:11:24 +0300

    Create layout.tsx

---
+++ app/hamburger/vrachi/[speciality]/layout.tsx
@@ -0,0 +1,10 @@
+/* path: app/hamburger/vrachi/[speciality]/layout.tsx */
+import type { ReactNode } from 'react';
+
+export const viewport = {
+  themeColor: '#f6f7fb',
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return children;
+}
