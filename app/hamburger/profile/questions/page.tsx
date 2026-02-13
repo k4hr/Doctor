d4d8c061e594e11cf7cbcf09570023e2d7d72e9f
k4hr <alecsandr-7a@yandex.ru>
2026-02-13 15:15:22 +0300

    Update page.tsx

--- app/hamburger/profile/questions/page.tsx
+++ app/hamburger/profile/questions/page.tsx
@@ -1,7 +1,7 @@
 /* path: app/hamburger/questions/page.tsx */
 'use client';
 
-import TopBarBack from '../../../components/TopBarBack';
+import TopBarBack from '../../../../components/TopBarBack';
 
 export default function HamburgerQuestionsPage() {
   return (
