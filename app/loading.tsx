8fe3fed3a8d59af8b05886825b8902a4bc789557
k4hr <alecsandr-7a@yandex.ru>
2026-02-16 02:10:44 +0300

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,23 @@
 /* path: app/loading.tsx */
 'use client';
 
+import { useEffect, useState } from 'react';
 import FullScreenLoader from '@/components/Loading/FullScreenLoader';
 
+function pickSplash() {
+  if (typeof window === 'undefined') return '/splash/doctor-9x16.jpg';
+  return window.innerWidth >= 900 ? '/splash/doctor-16x9.jpg' : '/splash/doctor-9x16.jpg';
+}
+
 export default function Loading() {
-  return <FullScreenLoader bgUrl="/splash/doctor.jpg" spinnerSize={70} />;
+  const [bgUrl, setBgUrl] = useState<string>(() => pickSplash());
+
+  useEffect(() => {
+    const onResize = () => setBgUrl(pickSplash());
+    onResize();
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+
+  return <FullScreenLoader bgUrl={bgUrl} spinnerSize={70} spinnerXPercent={50} spinnerYPercent={72} />;
 }
